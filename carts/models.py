4af0db9a0d092fc4332fb0ba8b355ea9d304cdac
sooj <soojksh@gmail.com>
2024-02-14 23:13:12 +0530

    pagination-views

--- carts/models.py
+++ carts/models.py
@@ -9,7 +9,7 @@ class Cart(models.Model):
     def __str__(self):
         return self.cart_id
     
-
+     
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
--- store/views.py
+++ store/views.py
@@ -3,8 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
-from django.http import HttpResponse
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -14,20 +13,27 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories,  is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products':products,
+        'products':paged_products,
         'product_count':product_count,
     }
     return render(request, 'store/store.html', context)
 
 
 def product_detail(request,category_slug, product_slug):
+    
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
