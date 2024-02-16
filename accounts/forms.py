d46849ed12cd5502f1059d9c9e78c9e831198d87
sooj <soojksh@gmail.com>
2024-02-16 20:05:28 +0530

    Registration template

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter password',
+    })) 
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm password',
+    }))
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number','email','password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
+ 
