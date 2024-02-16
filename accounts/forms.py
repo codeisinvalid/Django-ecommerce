f80033ace8cc05863a36106e0a1dba5bd6765577
sooj <soojksh@gmail.com>
2024-02-16 22:54:54 +0530

    User Registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,6 +16,10 @@ class RegistrationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder']='First Name'
+        self.fields['last_name'].widget.attrs['placeholder']='Last Name'
+        self.fields['email'].widget.attrs['placeholder']='Email'
+        self.fields['phone_number'].widget.attrs['placeholder']='Phone Number'
         for field in self.fields:
             self.fields[field].widget.attrs['class']='form-control'
  
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 
 def register(request):
-    form = RegistrationForm()
+    if request.method=="POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+
+            user = Account.object.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password= password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
+
     context = {
-        'form': form
+        'form': form,
     }
     return render(request, 'accounts/register.html', context)
 
