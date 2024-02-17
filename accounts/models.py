1036f33bc6e68f8d4aace54819250e9f1c2964f8
sooj <soojksh@gmail.com>
2024-02-18 00:27:57 +0530

    is_active retained to false before user verifies the email

--- accounts/models.py
+++ accounts/models.py
@@ -54,7 +54,7 @@ class Account(AbstractBaseUser):
     last_login = models.DateTimeField(auto_now_add = True)
     is_admin = models.BooleanField(default = False)
     is_staff = models.BooleanField(default = False)
-    is_active = models.BooleanField(default = True)
+    is_active = models.BooleanField(default = False)
     is_superadmin = models.BooleanField(default = False)
     is_verified=models.BooleanField(default= False)
 
@@ -62,7 +62,7 @@ class Account(AbstractBaseUser):
     REQUIRED_FIELDS= ['username', 'first_name', 'last_name']
 
     objects = MyAccountManager()
-
+    
     def __str__(self):
         return self.email
     
