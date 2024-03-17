a68b17d137671142981a4742d1da8fee74a98d38
sooj <soojksh@gmail.com>
2024-03-17 23:52:44 +0530

    general fixes and gategay integration: client

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -86,7 +86,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
