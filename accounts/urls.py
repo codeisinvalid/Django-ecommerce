519faef1d49296ecee83e82b2a54fdb7337108e7
sooj <soojksh@gmail.com>
2024-03-28 00:25:39 +0530

    edit-profile functionalities

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('resetpassword/', views.resetpassword, name='resetpassword'),
 
     path('my_orders/', views.my_orders, name="my_orders"),
-] 
+    path('edit_profile/', views.edit_profile, name="edit_profile"),
+]
