005731f4e113285f92c3e53556b8b3fb4414898b
sooj <soojksh@gmail.com>
2024-03-28 18:23:35 +0530

    product-detail multiple images functionality

--- accounts/views.py
+++ accounts/views.py
@@ -155,9 +155,12 @@ def activate(request, uidb64, token):
 @login_required(login_url = 'login')
 def dashboard(request):
     order = Order.objects.order_by('-created_at').filter(user_id = request.user.id, is_ordered=True)
-    orders_count = order.count
+    orders_count = order.count()
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    
     context={
-        'orders_count':orders_count,
+        'orders_count': orders_count,
+        'userprofile': userprofile,
     }
 
     return render (request, 'accounts/dashboard.html',context)
