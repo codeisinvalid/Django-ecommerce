3899593599b9cc4e25590038eeefd1b2c7b8b403
sooj <soojksh@gmail.com>
2024-03-28 14:57:21 +0530

    order-detail functionality

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
-from orders.models import Order
+from orders.models import Order, OrderProduct
 
 import requests
 
@@ -285,3 +285,19 @@ def change_password(request):
             return redirect('change_password')
 
     return render(request, "accounts/change_password.html")
+
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail=OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal=0
+    for i in order_detail:
+        subtotal += i.product_price*i.quantity
+    context={
+        'order_detail':order_detail,
+        'order':order,
+        'subtotal':subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
+
